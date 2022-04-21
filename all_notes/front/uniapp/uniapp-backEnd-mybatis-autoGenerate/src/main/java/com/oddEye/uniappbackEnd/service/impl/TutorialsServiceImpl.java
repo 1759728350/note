e66fa0a91c303dcb7693ca808ae27227d27ea093
dougax <1759728350@qq.com>
2022-04-21 22:43:22 +0800

    uniapp

---
+++ all_notes/front/uniapp/uniapp-backEnd-mybatis-autoGenerate/src/main/java/com/oddEye/uniappbackEnd/service/impl/TutorialsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.oddEye.uniappbackEnd.service.impl;/*
+ **description:
+ **param&method:
+ **caller:
+ **problems:
+ */
+
+import com.oddEye.uniappbackEnd.service.TutorialsService;
+
+public class TutorialsServiceImpl implements TutorialsService {
+
+}
